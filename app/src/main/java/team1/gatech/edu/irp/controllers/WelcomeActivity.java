ed6f4cf1d6431098fbb9656b62551d590f5c739c
gtg124x <gtg124x@gatech.edu>
2018-09-20 22:04:43 -0400

    Login Button send you to login screen

--- app/src/main/java/team1/gatech/edu/irp/controllers/WelcomeActivity.java
+++ app/src/main/java/team1/gatech/edu/irp/controllers/WelcomeActivity.java
@@ -5,11 +5,42 @@ import android.os.Bundle;
 
 import team1.gatech.edu.irp.R;
 
+import android.view.View;
+import team1.gatech.edu.irp.model.Model;
+import android.util.Log;
+import android.content.Intent;
+
+import android.widget.Button;
+
+
+
+/**
+ * THIS IS OUR TOP_LEVEL WINDOW THAT THE USER FIRST SEES IN THE APPLICATION!
+ *
+ * An activity representing login and register options.
+ *
+ */
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button btn = (Button) findViewById(R.id.login);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            }
+        });
+
     }
+
 }
+
+
+
+
